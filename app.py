dcb90984a25168c2589817f977a05276c41d43a2
unknown <charithak19@gmail.com>
2023-04-13 12:26:50 +0530

    added env

--- app.py
+++ app.py
@@ -28,21 +28,20 @@ def create_app(test_config=None):
     
     @app.route('/movies', methods=['GET'])
     def get_movies():
-        return "this is movies endpoint"
-        # try:
-        #     select_movies = Movie.query.order_by(Movie.id).all()   
-        #     format_movies = [movies.format() for movies in select_movies]
-        #     print("Select movies:",select_movies)
-        #     print("format movies:",format_movies)
-        #     if len(select_movies) ==0:
-        #         abort(404)
-        #     else:    
-        #         return jsonify({
-        #         'success':True,
-        #         'movies':format_movies
-        #     })
-        # except Exception as e:
-        #     print("get movies exception",e)
+        try:
+            select_movies = Movie.query.order_by(Movie.id).all()   
+            format_movies = [movies.format() for movies in select_movies]
+            print("Select movies:",select_movies)
+            print("format movies:",format_movies)
+            if len(select_movies) ==0:
+                abort(404)
+            else:    
+                return jsonify({
+                'success':True,
+                'movies':format_movies
+            })
+        except Exception as e:
+            print("get movies exception",e)
 
     return app
 
